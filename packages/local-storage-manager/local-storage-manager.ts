55860b4b361d7d308292a96795b969fbeced9da0
Eunsoo Lee <cbcruk@users.noreply.github.com>
2025-05-06 13:27:32 +0900

    update: LocalStorageManager 추가

---
+++ packages/local-storage-manager/local-storage-manager.ts
@@ -0,0 +1,16 @@
+class LocalStorageManager<T> {
+  constructor(private storageKey: string) {}
+
+  load(): T | null {
+    const data = localStorage.getItem(this.storageKey)
+    return data ? (JSON.parse(data) as T) : null
+  }
+
+  save(value: T) {
+    localStorage.setItem(this.storageKey, JSON.stringify(value))
+  }
+
+  remove() {
+    localStorage.removeItem(this.storageKey)
+  }
+}
